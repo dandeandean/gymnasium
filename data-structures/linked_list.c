93738298544d8e477f9534f90a27cf4edf988c5a
dandeandean <dpd5518@psu.edu>
2024-02-10 18:49:43 -0500

    starting data structures

---
+++ data-structures/linked_list.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define TRUE 1
+#define FALSE 1
+
+typedef struct{
+	struct lnode_t * next;
+	int value;
+	struct lnode_t * last;
+} lnode_t; // list node t
+
+int main(int argc, char *argv[]){
+	return 0;
+}
