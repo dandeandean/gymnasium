9d153aff5b60d5170ce7acaef4d84b56cc22a07f
dandeandean <dpd5518@psu.edu>
2024-02-10 21:41:25 -0500

    list coming along swimmingly

--- data-structures/linked_list.c
+++ data-structures/linked_list.c
@@ -1,13 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #define TRUE 1
-#define FALSE 1
+#define FALSE 0
+
+typedef struct Node{
+    int value;
+    struct Node * prev;
+    struct Node * next;
+} Node;
 
 typedef struct{
-	struct lnode_t * next;
-	int value;
-	struct lnode_t * last;
-} lnode_t; // list node t
+    Node * head;
+    Node * tail;
+    int size;
+}List;
+
+Node * node_init(int value, Node * next, Node * prev){
+    Node * node = (Node*)malloc(sizeof(Node));
+    node->value = value;
+    node->next = next;
+    node->prev = prev;
+    return node;
+}
+
+List * list_init(int first_val){	
+    List * list = (List*)malloc(sizeof(List));
+    Node * node = node_init(first_val,NULL,NULL);
+    list->size = 1;
+    list->head = node;
+    list->tail = node;
+    return list;
+}
+
+void free_list(List * list){
+    /*TODO go through and free all the nodes*/
+    free(list);
+}
+
+void print_list(List list){
+    printf("|");
+    Node * cur = list.head;
+    while (cur != NULL) {
+       printf("->(%d)",cur->value);
+       cur = cur->next;
+    } 
+    printf("\n");
+}
+void add_item(List * list,int value){
+    Node * new_node = node_init(value,NULL,list->tail);
+    list->tail->next = new_node;
+    list->tail = new_node;
+    list->size ++;
+}
 
 int main(int argc, char *argv[]){
-	return 0;
+    List * plist = list_init(69);
+    print_list(*plist);
+    add_item(plist,420);
+    print_list(*plist);
+    free_list(plist);
+    return 0;
 }
