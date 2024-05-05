79036e60aa6cd4070d865d261aa291e2e80ce5d5
DanDeanDean <dpd5518@psu.edu>
2024-05-05 09:20:34 -0400

    Inline one of the loops

--- algorithms/rust/longest-consec-seq-128/src/main.rs
+++ algorithms/rust/longest-consec-seq-128/src/main.rs
@@ -2,22 +2,19 @@ use std::collections::HashSet;
 
 pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
     let set: HashSet<&i32> = HashSet::from_iter(nums.iter());
-    let mut starts: Vec<&i32> = vec![];
+    let mut best_count = 0;
     for &num in set.iter() {
         if !set.contains(&(num - 1)) {
-            starts.push(num);
+            let mut count = 0;
+            while set.contains(&(num + count)) {
+                count += 1;
+            }
+            if count > best_count {
+                best_count = count;
+            }
         }
     }
-    let mut best_count: i32 = 0;
-    for &start in starts {
-        let mut count = 0;
-        while set.contains(&(start + count)) {
-            count += 1;
-        }
-        if count > best_count {
-            best_count = count;
-        }
-    }
+
     return best_count;
 }
 fn main() {
