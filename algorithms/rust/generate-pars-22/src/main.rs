f1b88fa2b6b12ecfe473a5baf4d953d2031c6acc
DanDeanDean <dpd5518@psu.edu>
2024-05-14 08:54:08 -0400

    leaving 22 for now

--- algorithms/rust/generate-pars-22/src/main.rs
+++ algorithms/rust/generate-pars-22/src/main.rs
@@ -1,11 +1,12 @@
+// based off of:  https://leetcode.com/problems/generate-parentheses/solutions/4570907/performance-optimized-backtrack-solution-in-rust-0ms-2-28-mb-64-memory
 fn generate(n: i32, s: &mut String, out: &mut Vec<String>) {
-    // let mut out: Vec<String> = vec![s.chars().collect()];
     let num_open = s.chars().filter(|c| *c == '(').count() as i32;
     let num_clos = s.chars().filter(|c| *c == ')').count() as i32;
     if n == num_clos && num_open == n {
         out.push(s.clone());
         return;
     };
+
     if num_open < n {
         s.push('(');
         generate(n, s, out);
