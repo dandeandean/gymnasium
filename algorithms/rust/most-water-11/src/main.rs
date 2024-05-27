1116817341d5adae008555326cb1667219106788
DanDeanDean <dpd5518@psu.edu>
2024-05-27 10:18:43 -0400

    passing in leetcode

--- algorithms/rust/most-water-11/src/main.rs
+++ algorithms/rust/most-water-11/src/main.rs
@@ -1,33 +1,29 @@
-fn calc_area(height: &Vec<i32>, i: usize, j: usize) -> i32 {
-    let mut cap = height[i];
-    if height[i] < cap {
-        cap = height[i];
+fn calc_area(height: &Vec<i32>, i: i32, j: i32) -> i32 {
+    let mut cap = height[j as usize];
+    if height[i as usize] < cap {
+        cap = height[i as usize];
     }
     let dist = (j as i32) - (i as i32);
-    cap * dist
+    let res = cap * dist;
+    res
 }
 pub fn max_area(height: Vec<i32>) -> i32 {
     let (mut left, mut right) = (0, height.len() - 1);
-    let mut bestsofar = calc_area(&height, left, right);
+    let mut bestsofar = calc_area(&height, left as i32, right as i32);
     while left < right {
+        let current_area = calc_area(&height, left as i32, right as i32);
+        if current_area > bestsofar {
+            bestsofar = current_area;
+        }
         if height[left] > height[right] {
             right -= 1;
-        }
-        if height[right] > height[left] {
+        } else {
             left += 1;
         }
-        if height[right] == height[left] {
-            right -= 1;
-            left += 1;
-        }
-        if right == left || left == height.len() {
-            break;
-        }
-        bestsofar = core::cmp::max(calc_area(&height, left, right), bestsofar);
     }
     bestsofar
 }
 fn main() {
-    //dbg!(max_area(vec![1, 8, 6, 2, 5, 4, 8, 3, 7])); // 49
+    dbg!(max_area(vec![1, 8, 6, 2, 5, 4, 8, 3, 7])); // 49
     dbg!(max_area(vec![1, 2])); // 1
 }
