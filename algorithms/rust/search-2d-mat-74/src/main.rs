b2c3d74ee253f965b1e4e1ee5bcdabd17626fecb
DanDeanDean <dpd5518@psu.edu>
2024-06-04 10:41:27 -0400

    chad response

--- algorithms/rust/search-2d-mat-74/src/main.rs
+++ algorithms/rust/search-2d-mat-74/src/main.rs
@@ -1,25 +1,10 @@
 pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
-    let nums: Vec<i32> = matrix
+    matrix
         .iter()
         .flat_map(|array| array.iter())
         .cloned()
-        .collect();
-    let (mut left, mut right) = (0, nums.len() - 1);
-    while left <= right {
-        let i = (left + right) / 2;
-        if nums[i] == target {
-            return true;
-        }
-        if nums[i] > target {
-            if i == 0 {
-                return false;
-            }
-            right = i - 1;
-        } else {
-            left = i + 1;
-        }
-    }
-    return false;
+        .collect::<Vec<i32>>()
+        .contains(&target)
 }
 fn main() {
     let matrix = vec![vec![1, 3, 5, 7], vec![10, 11, 16, 20], vec![23, 30, 34, 60]];
