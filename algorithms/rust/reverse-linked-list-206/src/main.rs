7e41378f85ce561bebdaba2c1f0fb79a483154ed
DanDeanDean <dpd5518@psu.edu>
2024-06-07 14:07:26 -0400

    less wacky solution for that one

--- algorithms/rust/reverse-linked-list-206/src/main.rs
+++ algorithms/rust/reverse-linked-list-206/src/main.rs
@@ -11,17 +11,14 @@ impl ListNode {
 }
 // from: https://leetcode.com/problems/reverse-linked-list/solutions/4904416/beats-100-full-solution-explained-with-dry-run-java-c-python-rust-javascript-go/
 pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    let mut dummy = None;
-    let mut current = head;
-    while let Some(mut node) = current {
-        let next = node // already unwrapped from the Some( ..)
-            .next
-            .take(); //turns node into none
-        dbg!(&node);
-        node.next = dummy.take();
-        dummy = Some(node);
-        current = next;
+    let mut curr: Option<Box<ListNode>> = head;
+    let mut prev: Option<Box<ListNode>> = None;
+    while let Some(mut node) = curr {
+        let holder = node.next;
+        node.next = prev;
+        prev = Some(node);
+        curr = holder;
     }
-    dummy
+    curr
 }
 fn main() {}
