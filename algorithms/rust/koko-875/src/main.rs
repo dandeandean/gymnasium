4f0483d6c33ff3c861c41bba4b6af7bb310add32
DanDeanDean <dpd5518@psu.edu>
2024-06-09 10:58:11 -0400

    brute force 875, too slow :(

--- algorithms/rust/koko-875/src/main.rs
+++ algorithms/rust/koko-875/src/main.rs
@@ -1,21 +1,37 @@
-pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
-    let (mut l, &(mut r)) = (1, piles.iter().max().unwrap());
-    let mut ret = r;
-    while l < r {
-        let k = (l + r) / 2;
-        let mut tt: f32 = 0.0;
-        for &p in &piles {
-            tt += (p as f32) / (k as f32);
-        }
-        if tt < h as f32 {
-            ret = k;
-            r = k - 1;
+fn calc_h(piles: &Vec<i32>, k: i32) -> i32 {
+    let mut ret = 0;
+    for &p in piles {
+        if p < k {
+            ret += 1;
         } else {
-            l = k + 1;
+            let mut res: i32 = p / k;
+            // check if there's some left over
+            if (p % k) != 0 {
+                res += 1;
+            }
+            ret += res;
         }
     }
     ret
 }
+pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
+    //let (_, &(mut r)) = (1, piles.iter().max().unwrap());
+    let &(mut ret) = piles.iter().max().unwrap();
+    while calc_h(&piles, ret) <= h {
+        ret -= 1;
+    }
+    ret + 1
+}
+
 fn main() {
     dbg!(min_eating_speed(vec![3, 6, 7, 11], 8));
+    dbg!(min_eating_speed(vec![30, 11, 23, 4, 20], 5));
+    dbg!(min_eating_speed(vec![30, 11, 23, 4, 20], 6));
+    let big = vec![
+        332484035, 524908576, 855865114, 632922376, 222257295, 690155293, 112677673, 679580077,
+        337406589, 290818316, 877337160, 901728858, 679284947, 688210097, 692137887, 718203285,
+        629455728, 941802184,
+    ];
+    let large = 823855818;
+    dbg!(min_eating_speed(big, large));
 }
