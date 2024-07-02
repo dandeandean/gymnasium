291e0bde2c1cc00d5591b26853577daa5f52dea4
DanDeanDean <dpd5518@psu.edu>
2024-07-02 18:01:19 -0500

    424 different approach

--- algorithms/rust/sliding-window/longest-w-replacement-424/src/main.rs
+++ algorithms/rust/sliding-window/longest-w-replacement-424/src/main.rs
@@ -1,18 +1,18 @@
 pub fn character_replacement(s: String, k: i32) -> i32 {
-    let (mut l, mut r, mut best): (usize, usize, i32) = (0, 1, 0);
-    let mut mk = k;
-    while r < s.len() {
-        if s.chars().nth(l) != s.chars().nth(r) {
-            mk -= 1;
-        }
-        if mk < 0 {
+    let (mut l, mut best): (usize, i32) = (1, 0);
+    let mut counts: std::collections::HashMap<char, i32> = std::collections::HashMap::new();
+    let mut maxf: i32;
+    //while r < s.len() {
+    for (r, c) in s.chars().enumerate() {
+        *counts.entry(c).or_insert(0) += 1;
+        maxf = std::cmp::max(*counts.values().into_iter().max().unwrap(), counts[&c]);
+        if r as i32 - l as i32 - maxf + 1 > k {
+            *counts.entry(c).or_insert(0) -= 1;
             l += 1;
         }
-        best = std::cmp::max(best, 1 + r as i32 - l as i32);
-        dbg!(r, l);
-        r += 1;
+        best = std::cmp::max(best, r as i32 - l as i32 + 1);
     }
-    std::cmp::max(best, 1 + r as i32 - l as i32)
+    best
 }
 fn main() {
     dbg!(
