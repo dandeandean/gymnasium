89a398094d935adbbf857942b3f41ab528a50a1f
DanDeanDean <dpd5518@psu.edu>
2024-07-11 21:08:24 -0400

    starting 76

--- algorithms/rust/sliding-window/min-window-76/src/main.rs
+++ algorithms/rust/sliding-window/min-window-76/src/main.rs
@@ -1,4 +1,13 @@
 pub fn min_window(s: String, t: String) -> String {
+    if t == "".to_string() {
+        return t;
+    }
+    let mut t_hash: std::collections::HashMap<char, i32> =
+        std::collections::HashMap::with_capacity(t.len());
+    dbg!(t_hash.len());
+    for c in t.chars() {
+        *t_hash.entry(c).or_insert(0) += 1;
+    }
     "abc".to_string()
 }
 fn main() {
