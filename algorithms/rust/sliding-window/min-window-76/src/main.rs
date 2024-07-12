0b5b908b60b01e562a7fad3a643935c2331b25e4
DanDeanDean <dpd5518@psu.edu>
2024-07-12 14:33:29 -0400

    76 still trying to figure it all out

--- algorithms/rust/sliding-window/min-window-76/src/main.rs
+++ algorithms/rust/sliding-window/min-window-76/src/main.rs
@@ -4,11 +4,12 @@ pub fn min_window(s: String, t: String) -> String {
     }
     let mut t_hash: std::collections::HashMap<char, i32> =
         std::collections::HashMap::with_capacity(t.len());
-    dbg!(t_hash.len());
     for c in t.chars() {
         *t_hash.entry(c).or_insert(0) += 1;
     }
-    "abc".to_string()
+    let mut s_hash: std::collections::HashMap<char, i32> =
+        std::collections::HashMap::with_capacity(t_hash.len());
+    let (have, need): (i32, i32) = (0, t_hash.len() as i32);
 }
 fn main() {
     dbg!(
