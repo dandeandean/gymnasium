32e95116b1c06fea88e574b595f5d8547ba45aef
DanDeanDean <dpd5518@psu.edu>
2024-07-15 16:06:15 -0400

    not working

--- algorithms/rust/sliding-window/sliding-window-max-239/src/main.rs
+++ algorithms/rust/sliding-window/sliding-window-max-239/src/main.rs
@@ -1,19 +1,28 @@
 pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
     let mut out: Vec<i32> = vec![];
     let (mut l, mut r) = (0, (k - 1) as usize);
-    let mut q: std::collections::VecDeque<i32> = std::collections::VecDeque::new();
+    let mut q: std::collections::VecDeque<usize> = std::collections::VecDeque::new();
     while r < nums.len() {
         // there's gotta be a better way of doing this ......
         while !q.is_empty() && nums[*q.iter().last().unwrap() as usize] < nums[r] {
             //
             q.pop_back();
         }
-        q.push_back(r as i32);
+        q.push_back(r);
         if &l > q.get(0).unwrap() {
             q.pop_front();
         }
+        if (r + 1) as i32 >= k {
+            out.push(nums[*q.get(0).unwrap()]);
+            l += 1;
+        }
+        r += 1;
     }
     out
 }
-fn main() {}
-
+fn main() {
+    dbg!(
+        max_sliding_window(vec![1, 3, -1, -3, 5, 3, 6, 7], 3),
+        max_sliding_window(vec![1], 1)
+    );
+}
