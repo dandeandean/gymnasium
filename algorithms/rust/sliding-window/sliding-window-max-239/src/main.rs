efae94e26439174d996dcc9da6d8d382962721f1
DanDeanDean <dpd5518@psu.edu>
2024-07-16 09:05:58 -0400

    fix smart solution, add dumb solution

--- algorithms/rust/sliding-window/sliding-window-max-239/src/main.rs
+++ algorithms/rust/sliding-window/sliding-window-max-239/src/main.rs
@@ -1,11 +1,10 @@
 pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
     let mut out: Vec<i32> = vec![];
-    let (mut l, mut r) = (0, (k - 1) as usize);
+    let (mut l, mut r) = (0, 0);
     let mut q: std::collections::VecDeque<usize> = std::collections::VecDeque::new();
     while r < nums.len() {
         // there's gotta be a better way of doing this ......
         while !q.is_empty() && nums[*q.iter().last().unwrap() as usize] < nums[r] {
-            //
             q.pop_back();
         }
         q.push_back(r);
@@ -20,9 +19,22 @@ pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
     }
     out
 }
+
+pub fn dumb_max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    // too  slow to pass leetcode
+    let mut out: Vec<i32> = vec![];
+    let (mut l, mut r) = (0, (k) as usize);
+    // LEQ || l..=r
+    while r <= nums.len() {
+        out.push(*nums[l..r].iter().max().unwrap());
+        l += 1;
+        r += 1;
+    }
+    out
+}
 fn main() {
     dbg!(
-        max_sliding_window(vec![1, 3, -1, -3, 5, 3, 6, 7], 3),
-        max_sliding_window(vec![1], 1)
+        dumb_max_sliding_window(vec![1, 3, -1, -3, 5, 3, 6, 7], 3),
+        dumb_max_sliding_window(vec![1], 1)
     );
 }
