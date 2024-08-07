147fc08915c72459c0c691b9e022618598f1efa8
dandeandean <dpd5518@psu.edu>
2024-08-07 09:18:24 -0400

    add mysterious solution for 150

--- algorithms/neetcode-150/linked-list/dup-num-287/python-solution/main.py
+++ algorithms/neetcode-150/linked-list/dup-num-287/python-solution/main.py
@@ -8,4 +8,21 @@ class Solution:
         return -1
 
     def findDuplicate(self, nums: list[int]) -> int:
-        return -1
+        fast, slow = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if fast == slow:
+                break
+        fast = 0
+        while fast != slow:
+            slow = nums[slow]
+            fast = nums[fast]
+        return fast
+
+if __name__ == "__main__":
+    nums = [ 1, 2, 3, 3, 4]
+    nums2 = [1,3,4,2,2]
+    s = Solution()
+    print(s.findDuplicate(nums))
+    print(s.findDuplicate(nums2))
