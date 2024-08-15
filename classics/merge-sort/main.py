ab59d18508be523e509c1070350cc08404ff9b69
DanDeanDean <dpd5518@psu.edu>
2024-08-15 15:00:59 -0400

    add merge

--- classics/merge-sort/main.py
+++ classics/merge-sort/main.py
@@ -1,16 +1,29 @@
-def __recurse(l1: list[int], l2: list[int]):
-    if len(l1) == len(l2) == 1:
-        return [l1[0], l2[0]] if l1[0] < l2[0] else [l2[0], l1[0]]
-    l1_fh = l1[:len(l1)//2]
-    l1_sh = l1[len(l1)//2:]
-    l2_fh = l1[:len(l1)//2]
-    l2_sh = l1[len(l1)//2:]
-    return __recurse(l1_fs, l2_fs)
+def __merge(l1: list[int], l2: list[int]):
+    """
+        returns sorted list if l1 & l2 are sorted
+    """
+    out: list[int] = []
+    i1, i2 = 0, 0
+    while i1 < len(l1) and i2 < len(l2):
+        if l1[i1] < l2[i2]:
+            out.append(l1[i1])
+            i1 += 1
+        else:
+            out.append(l2[i2])
+            i2 += 1
+    while i1 < len(l1):
+        out.append(l1[i1])
+        i1 += 1
+    while i2 < len(l2):
+        out.append(l2[i2])
+        i2 += 1
+    return out
+
 
 def merge_sort(nums: list[int]) -> list[int]:
     # break up the list into separate pieces & zip them back together correctly
     return [nums[0]]
 
 if __name__ == "__main__":
-    print(__recurse([2],[1]))
+    print(__merge([3,4],[2,3]))
     
