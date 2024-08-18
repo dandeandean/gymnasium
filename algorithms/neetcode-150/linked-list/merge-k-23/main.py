636c28b973e36b517fe277a86733437beb3b430d
DanDeanDean <dpd5518@psu.edu>
2024-08-18 11:30:00 -0400

    add 23, minimal understanding

--- algorithms/neetcode-150/linked-list/merge-k-23/main.py
+++ algorithms/neetcode-150/linked-list/merge-k-23/main.py
@@ -3,11 +3,16 @@ class ListNode:
         self.val = val
         self.next = next
 
-class Solution:
-    def mergeKLists(self, lists: list[ListNode]) -> ListNode | None:
-        if len(lists) <= 1:
-            return lists[0] if len(lists) == 1 else None
+    def __repr__(self) -> str:
+        outstr = f"({self.val}){'->' if self.next else ''}"
+        cur = self.next
+        while cur:
+            outstr += f"({cur.val}){'->' if cur.next else ''}"
+            cur = cur.next
+        return outstr
+    __str__ = __repr__
 
+class Solution:
     def mergeList(self, l1:ListNode | None, l2: ListNode | None) -> ListNode | None:
         anchor: ListNode = ListNode()
         cur: ListNode | None = anchor
@@ -26,4 +31,30 @@ class Solution:
         return anchor.next
 
 
+    def mergeKLists(self, lists: list[ListNode]) -> ListNode | None:
+        if len(lists) <= 1:
+            return lists[0] if len(lists) == 1 else None
+        while len(lists) > 1:
+            merged_so_far = []
+            print("inside",lists)
+            for i in range(0,len(lists), 2):
+                print("\tmerged", merged_so_far)
+                l1 = lists[i]
+                l2 = lists[i+1] if (i + 1) < len(lists) else None
+                merged_so_far.append(self.mergeList(l1,l2))
+            lists = merged_so_far
+        return lists[0]
+            
+
+if __name__ == "__main__":
+    LN = ListNode 
+    l1 = LN(1,LN(2,LN(3)))
+    l2 = LN(4,LN(5,LN(6)))
+    l3 = LN(5,LN(5,LN(6)))
+    ls = [l1, l2, l3]
+    print(ls)
+    s = Solution()
+    merged = s.mergeKLists(ls)
+    print(merged)
+    
 
