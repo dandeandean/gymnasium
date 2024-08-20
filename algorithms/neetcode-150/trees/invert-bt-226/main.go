1e88a9b9527880eea6eac08ab880b0b65f14768c
DanDeanDean <dpd5518@psu.edu>
2024-08-20 16:24:34 -0400

    invert bt lightwork

---
+++ algorithms/neetcode-150/trees/invert-bt-226/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+/**
+ * Definition for a binary tree node.
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func simpleSwap(cur *TreeNode) {
+	oldLeft := cur.Left
+	cur.Left = cur.Right
+	cur.Right = oldLeft
+}
+func invertTree(root *TreeNode) *TreeNode {
+	if nil == root || (nil == root.Left && nil == root.Right) {
+		return root
+	}
+	simpleSwap(root)
+	if nil != root.Left {
+		invertTree(root.Left)
+	}
+	if nil != root.Right {
+		invertTree(root.Right)
+	}
+	return root
+}
+func main() {
+	fmt.Print("HLW")
+}
