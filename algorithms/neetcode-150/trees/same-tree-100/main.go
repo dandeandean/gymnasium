8a4c7ff94a8181d95a8f76d444764c9358f8addd
DanDeanDean <dpd5518@psu.edu>
2024-08-24 06:00:41 -0400

    add lc solution for 100

--- algorithms/neetcode-150/trees/same-tree-100/main.go
+++ algorithms/neetcode-150/trees/same-tree-100/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type TreeNode struct {
 	Val   int
@@ -23,6 +25,16 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 		return false
 	}
 	return true
+}
+
+func isSameTreeLC(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil || p.Val != q.Val {
+		return false
+	}
+	return isSameTreeLC(p.Left, q.Left) && isSameTreeLC(p.Right, q.Right)
 }
 
 func main() {
