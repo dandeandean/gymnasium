59e9498fbcdbfae0a2143e9cbf53f71861dfdb62
Dan Dean <DPD5518@psu.edu>
2024-08-27 19:35:10 -0400

    start 150

---
+++ algorithms/neetcode-150/trees/order-traversal-102/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	x := 10
+	out := make([][]int, x)
+	return out
+}
+func main() {
+	fmt.Println("Hallo Leute!")
+}
