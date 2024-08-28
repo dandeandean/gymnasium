cfc625d1a3cdfb77304618f3c65b3ed5e91dba6b
Dan Dean <DPD5518@psu.edu>
2024-08-28 06:54:53 -0400

    untested 102

--- algorithms/neetcode-150/trees/order-traversal-102/main.go
+++ algorithms/neetcode-150/trees/order-traversal-102/main.go
@@ -9,9 +9,29 @@ type TreeNode struct {
 }
 
 func levelOrder(root *TreeNode) [][]int {
-	x := 10
-	out := make([][]int, x)
+	var out [][]int
+	q := make([]*TreeNode, 0)
+	if root != nil {
+		q = append(q, root)
+	}
+	for len(q) != 0 {
+		vals := make([]int, 0)
+		for i := 0; i < len(q); i++ {
+			node := q[0]
+			q = q[1:]
+			vals = append(vals, node.Val)
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+			out = append(out, vals)
+		}
+
+	}
 	return out
+
 }
 func main() {
 	fmt.Println("Hallo Leute!")
