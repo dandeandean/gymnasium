c73e8e4d099e84de96a67e2ff28fd8e1d176f2e6
DanDeanDean <dpd5518@psu.edu>
2024-09-04 06:04:45 -0400

    remake right side

--- algorithms/neetcode-150/trees/right-side-199/main.go
+++ algorithms/neetcode-150/trees/right-side-199/main.go
@@ -14,8 +14,24 @@ func rightSideView(root *TreeNode) []int {
 	q := make([]*TreeNode, 0)
 	q = append(q, root)
 	for len(q) > 0 {
-		out = append(out, root.Val)
-		root = root.Right
+		level := make([]int, 0)
+		lenQ := len(q)
+		for i := 0; i < lenQ; i++ {
+			youAreHere := q[0]
+			q = q[1:]
+			if youAreHere != nil {
+				level = append(level, youAreHere.Val)
+				if youAreHere.Left != nil {
+					q = append(q, youAreHere.Left)
+				}
+				if youAreHere.Right != nil {
+					q = append(q, youAreHere.Right)
+				}
+			}
+		}
+		if len(level) != 0 {
+			out = append(out, level[len(level)-1])
+		}
 	}
 	return out
 }
