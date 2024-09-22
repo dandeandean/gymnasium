794323cb0336d2124b027854756f8805ac44d7d3
DanDeanDean <dpd5518@psu.edu>
2024-09-22 12:17:26 -0400

    correct answer

--- algorithms/neetcode-150/trees/max-path-sum-124/main.go
+++ algorithms/neetcode-150/trees/max-path-sum-124/main.go
@@ -6,39 +6,31 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func dfs(node *TreeNode, sum *int) int {
 	if node == nil {
 		return 0
 	}
-	leftSide := dfs(node.Left, sum)
-	rightSide := dfs(node.Right, sum)
-	if leftSide < 0 {
-		leftSide = 0
-	}
-	if rightSide < 0 {
-		rightSide = 0
-	}
-	res := node.Val + leftSide + rightSide
-	if res < 0 {
-		res = 0
-	}
-
-	extra := 0
-	if leftSide > rightSide {
-		extra = leftSide
-	}
-	extra = rightSide
-
-	return node.Val + extra
+	leftSide := max(dfs(node.Left, sum), 0)
+	rightSide := max(dfs(node.Right, sum), 0)
+	*sum = max(*sum, node.Val+leftSide+rightSide)
+	return node.Val + max(leftSide, rightSide)
 }
+
 func maxPathSum(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
 	sum := root.Val
-	dfs(root.Left, &sum)
-	dfs(root.Right, &sum)
+	dfs(root, &sum)
 	return sum
 }
+
 func main() {
 }
