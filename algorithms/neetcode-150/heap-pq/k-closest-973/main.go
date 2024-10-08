e7287fa8dc0bd905fb57e4aca06869a687f11fe8
Dan Dean <dandean44523@gmail.com>
2024-10-08 07:03:56 -0400

    completely untested solution

--- algorithms/neetcode-150/heap-pq/k-closest-973/main.go
+++ algorithms/neetcode-150/heap-pq/k-closest-973/main.go
@@ -2,15 +2,35 @@ package main
 
 import "fmt"
 
+type Coord []int
+
+func (c Coord) SquardDist() int {
+	X := c[0]
+	Y := c[1]
+	return X*X + Y*Y
+}
+func (c Coord) Slice() []int {
+	return []int{c[0], c[1]}
+}
+
+func kClosest(points [][]int, k int) [][]int {
+	h := MinHeap()
+	m := make(map[int]Coord)
+	for _, point := range points {
+		c := make(Coord, 2)
+		c[0], c[1] = point[0], point[1]
+		m[c.SquardDist()] = c
+		h.Push(c.SquardDist())
+	}
+	out := make([][]int, k)
+	for i := 0; i < k; i++ {
+		sq := h.Pop()
+		// this may just unpack the slice???
+		out = append(out, m[sq].Slice())
+	}
+	return out
+}
+
 func main() {
-	h := MaxHeap()
-	h.From([]int{9, 2, 8, 5})
-	fmt.Println(h.Vals)
-	h2 := MinHeap()
-	h2.From([]int{9, 2, 8, 5})
-	fmt.Println(h2.Vals)
-	fmt.Println(
-		h.IsMax,
-		h2.IsMax,
-	)
+	fmt.Println("")
 }
