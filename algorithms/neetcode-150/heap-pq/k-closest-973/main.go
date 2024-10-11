6bd970a0dac67df9fe521c2a975a32481794a6dc
Dan Dean <dandean44523@gmail.com>
2024-10-11 06:24:37 -0400

    must use heap from containers

--- algorithms/neetcode-150/heap-pq/k-closest-973/main.go
+++ algorithms/neetcode-150/heap-pq/k-closest-973/main.go
@@ -1,17 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"container/heap"
+	"fmt"
+)
 
+type Point struct {
+	X      int
+	Y      int
+	DistSq int
+}
+
+func makePoint(x, y int) Point {
+	return Point{
+		X:      x,
+		Y:      y,
+		DistSq: x*x + y*y,
+	}
+}
+
+func (p Point) Unpack() []int {
+	return []int{p.X, p.Y}
+}
+
+type PQ []Point
+
+func (h PQ) Len() int            { return len(h) }
+func (h PQ) Less(i, j int) bool  { return h[i].DistSq < h[j].DistSq }
+func (h PQ) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *PQ) Push(x interface{}) { *h = append(*h, x.(Point)) }
+func (h *PQ) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
 func kClosest(points [][]int, k int) [][]int {
-	h := MinHeap()
-	for _, point := range points {
-		c := make(Coord, 2)
-		c[0], c[1] = point[0], point[1]
-		h.Push(c)
+	pq := &PQ{}
+	for _, coord := range points {
+		p := makePoint(coord[0], coord[1])
+		heap.Push(pq, p)
 	}
 	out := make([][]int, k)
 	for i := range out {
-		out[i] = h.Pop()
+		out[i] = heap.Pop(pq).(Point).Unpack()
 	}
 	return out
 }
