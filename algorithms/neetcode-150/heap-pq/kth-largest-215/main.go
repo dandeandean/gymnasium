b51c05d4b9fbb47b2fb71afc533059fe6c287e41
Dan Dean <dandean44523@gmail.com>
2024-10-14 06:27:39 -0400

    add 215 solution

--- algorithms/neetcode-150/heap-pq/kth-largest-215/main.go
+++ algorithms/neetcode-150/heap-pq/kth-largest-215/main.go
@@ -1,15 +1,28 @@
 package main
 
-type MaxHeap []int
+import "container/heap"
 
-func (h MaxHeap) Len() int            { return len(h) }
-func (h MaxHeap) Less(i, j int) bool  { return h[i] < h[j] }
-func (h MaxHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
-func (h *MaxHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
-func (h *MaxHeap) Pop() interface{} {
+type MinHeap []int
+
+func (h MinHeap) Len() int            { return len(h) }
+func (h MinHeap) Less(i, j int) bool  { return h[i] > h[j] } // gt for max heap
+func (h MinHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *MinHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h *MinHeap) Pop() interface{} {
 	old := *h
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
 	return x
+}
+func findKthLargest(nums []int, k int) int {
+	mh := &MinHeap{}
+	for _, n := range nums {
+		heap.Push(mh, n)
+	}
+	for i := 0; i < k-1; i++ {
+		heap.Pop(mh)
+	}
+	out := heap.Pop(mh).(int)
+	return out
 }
