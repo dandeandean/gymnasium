cd889b59ed4eae93d975afc60fccb4ce4702232a
Dan Dean <dandean44523@gmail.com>
2024-10-15 06:14:43 -0400

    fix max-> min

--- algorithms/neetcode-150/heap-pq/kth-largest-215/main.go
+++ algorithms/neetcode-150/heap-pq/kth-largest-215/main.go
@@ -2,13 +2,13 @@ package main
 
 import "container/heap"
 
-type MinHeap []int
+type MaxHeap []int
 
-func (h MinHeap) Len() int            { return len(h) }
-func (h MinHeap) Less(i, j int) bool  { return h[i] > h[j] }
-func (h MinHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
-func (h *MinHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
-func (h *MinHeap) Pop() interface{} {
+func (h MaxHeap) Len() int            { return len(h) }
+func (h MaxHeap) Less(i, j int) bool  { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *MaxHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h *MaxHeap) Pop() interface{} {
 	old := *h
 	n := len(old)
 	x := old[n-1]
@@ -16,7 +16,7 @@ func (h *MinHeap) Pop() interface{} {
 	return x
 }
 func findKthLargest(nums []int, k int) int {
-	mh := &MinHeap{}
+	mh := &MaxHeap{}
 	for _, n := range nums {
 		heap.Push(mh, n)
 	}
