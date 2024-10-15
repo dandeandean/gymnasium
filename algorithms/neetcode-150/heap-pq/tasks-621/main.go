220de6cef20f57eff686199de35758e18051610d
Dan Dean <dandean44523@gmail.com>
2024-10-15 06:11:54 -0400

    set up

--- algorithms/neetcode-150/heap-pq/tasks-621/main.go
+++ algorithms/neetcode-150/heap-pq/tasks-621/main.go
@@ -1,11 +1,9 @@
 package main
 
-import "container/heap"
-
 type MinHeap []int
 
 func (h MinHeap) Len() int            { return len(h) }
-func (h MinHeap) Less(i, j int) bool  { return h[i] > h[j] }
+func (h MinHeap) Less(i, j int) bool  { return h[i] > h[j] } // gt for max heap
 func (h MinHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
 func (h *MinHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
 func (h *MinHeap) Pop() interface{} {
@@ -15,14 +13,6 @@ func (h *MinHeap) Pop() interface{} {
 	*h = old[0 : n-1]
 	return x
 }
-func findKthLargest(nums []int, k int) int {
-	mh := &MinHeap{}
-	for _, n := range nums {
-		heap.Push(mh, n)
-	}
-	for i := 0; i < k-1; i++ {
-		heap.Pop(mh)
-	}
-	out := heap.Pop(mh).(int)
-	return out
+func leastInterval(tasks []byte, n int) int {
+	return 42
 }
