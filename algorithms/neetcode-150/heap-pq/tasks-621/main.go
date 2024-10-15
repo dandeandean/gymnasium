91c5f6fd47e23c23edffbe623f82001dcb033329
Dan Dean <dandean44523@gmail.com>
2024-10-15 06:50:24 -0400

    add some for 621

--- algorithms/neetcode-150/heap-pq/tasks-621/main.go
+++ algorithms/neetcode-150/heap-pq/tasks-621/main.go
@@ -1,12 +1,17 @@
 package main
 
-type MinHeap []int
+import (
+	"container/heap"
+	"fmt"
+)
 
-func (h MinHeap) Len() int            { return len(h) }
-func (h MinHeap) Less(i, j int) bool  { return h[i] > h[j] } // gt for max heap
-func (h MinHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
-func (h *MinHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
-func (h *MinHeap) Pop() interface{} {
+type MaxHeap []int
+
+func (h MaxHeap) Len() int            { return len(h) }
+func (h MaxHeap) Less(i, j int) bool  { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *MaxHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h *MaxHeap) Pop() interface{} {
 	old := *h
 	n := len(old)
 	x := old[n-1]
@@ -14,5 +19,22 @@ func (h *MinHeap) Pop() interface{} {
 	return x
 }
 func leastInterval(tasks []byte, n int) int {
+	h := &MaxHeap{}
+	//q := make([]int, 0)
+	count := make(map[byte]int, 0)
+	for _, t := range tasks {
+		count[t]++
+	}
+	for _, c := range count {
+		heap.Push(h, c)
+	}
+	fmt.Println(count, n)
+	fmt.Println(h, n)
 	return 42
+}
+
+func main() {
+	tasks := []byte{'A', 'B', 'A'}
+	leastInterval(tasks, 1)
+
 }
