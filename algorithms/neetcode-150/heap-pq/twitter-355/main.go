2e0816b89d27cb07306f3fcb664ed142053daba0
Dan Dean <dandean44523@gmail.com>
2024-10-25 05:55:58 -0400

    add main

--- algorithms/neetcode-150/heap-pq/twitter-355/main.go
+++ algorithms/neetcode-150/heap-pq/twitter-355/main.go
@@ -1,10 +1,13 @@
 package main
 
+import "fmt"
+
 type Set map[int]bool
 
 type Tweet struct {
-	Time int
-	ID   int
+	Time   int
+	ID     int
+	UserID int
 }
 
 type Twitter struct {
@@ -15,14 +18,19 @@ type Twitter struct {
 }
 
 func Constructor() Twitter {
-	X := Twitter{}
+	X := Twitter{
+		Time:        0,
+		Users:       make(Set),
+		Tweets:      make(map[int][]Tweet),
+		IsFollowing: make(map[int]Set),
+	}
 	return X
 }
 
 func (this *Twitter) PostTweet(userId int, tweetId int) {
 	this.Tweets[userId] = append(
 		this.Tweets[userId],
-		Tweet{Time: this.Time, ID: tweetId},
+		Tweet{Time: this.Time, ID: tweetId, UserID: userId},
 	)
 	this.Time++
 }
@@ -30,18 +38,17 @@ func (this *Twitter) PostTweet(userId int, tweetId int) {
 func (this *Twitter) GetNewsFeed(userId int) []int {
 	following := this.IsFollowing[userId]
 	allTweets := make([]Tweet, 0)
+	out := make([]int, 0)
 	for k, v := range following {
 		if v {
 			allTweets = append(allTweets, this.Tweets[k]...) // ... means slice to slice or something
 		}
 	}
-	this.Time++
-	return []int{69}
+	return out
 }
 
 func (this *Twitter) Follow(followerId int, followeeId int) {
 	this.IsFollowing[followerId][followeeId] = true
-	this.Time++
 }
 
 func (this *Twitter) Unfollow(followerId int, followeeId int) {
@@ -50,14 +57,20 @@ func (this *Twitter) Unfollow(followerId int, followeeId int) {
 		this.IsFollowing[followerId],
 		followeeId,
 	)
-	this.Time++
 }
 
-/**
- * Your Twitter object will be instantiated and called as such:
- * obj := Constructor();
- * obj.PostTweet(userId,tweetId);
- * param_2 := obj.GetNewsFeed(userId);
- * obj.Follow(followerId,followeeId);
- * obj.Unfollow(followerId,followeeId);
- */
+func main() {
+	/**
+	 * Your Twitter object will be instantiated and called as such:
+	 * obj := Constructor();
+	 * obj.PostTweet(userId,tweetId);
+	 * param_2 := obj.GetNewsFeed(userId);
+	 * obj.Follow(followerId,followeeId);
+	 * obj.Unfollow(followerId,followeeId);
+	 */
+	obj := Constructor()
+	obj.PostTweet(0, 0)
+	param_2 := obj.GetNewsFeed(0)
+	fmt.Println(obj, param_2)
+
+}
