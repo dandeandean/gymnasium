c89678cc9d1d97109029b3e5b75645f88cb297f9
Dan Dean <dandean44523@gmail.com>
2024-10-29 07:07:01 -0400

    not working :(

--- algorithms/neetcode-150/heap-pq/median-295/main.go
+++ algorithms/neetcode-150/heap-pq/median-295/main.go
@@ -17,51 +17,50 @@ func Constructor() MedianFinder {
 	}
 }
 
-func (this *MedianFinder) diffPiles() int {
+func (this *MedianFinder) diffPiles() (int, *MayHeap, *MayHeap) {
+	/// returns diff, larger, smaller
 	raw := this.SmallPile.Len() - this.BigPile.Len()
 	if raw < 0 {
-		return -raw
+		return -raw, this.BigPile, this.SmallPile
 	}
-	return raw
+	return raw, this.SmallPile, this.BigPile
 }
 
 func (this *MedianFinder) AddNum(num int) {
 	heap.Push(this.SmallPile, num)
-	if this.BigPile.Len() > 0 && this.BigPile.Peek().(int) < this.SmallPile.Peek().(int) {
-		out := heap.Pop(this.SmallPile)
-		heap.Push(this.BigPile, out)
+	if this.BigPile.Len() > 0 {
+		if this.BigPile.Peek().(int) < this.SmallPile.Peek().(int) {
+			out := heap.Pop(this.SmallPile)
+			heap.Push(this.BigPile, out)
+		}
 	}
-	if this.BigPile.Len() < this.SmallPile.Len()+1 {
-		out := heap.Pop(this.SmallPile)
-		heap.Push(this.BigPile, out)
-	}
-	if this.SmallPile.Len() > this.BigPile.Len()+1 {
-		out := heap.Pop(this.BigPile)
-		heap.Push(this.SmallPile, out)
+	nDiff, larger, smaller := this.diffPiles()
+	if nDiff > 1 {
+		heap.Push(smaller, heap.Pop(larger))
 	}
 
 }
 
 func (this *MedianFinder) FindMedian() float64 {
-	if this.diffPiles() == 0 {
+	nDiff, _, _ := this.diffPiles()
+	if nDiff == 0 {
 		small, big := float64(this.SmallPile.Peek().(int)), float64(this.BigPile.Peek().(int))
 		return (small + big) / 2.0
 	}
-	correctPile := this.SmallPile
-	if this.BigPile.Len() > this.SmallPile.Len() {
-		correctPile = this.BigPile
+	if this.SmallPile.Len() > 0 {
+		return float64(this.SmallPile.Peek().(int))
 	}
-	return float64(correctPile.Peek().(int))
+	return float64(this.BigPile.Peek().(int))
 }
 
 func main() {
 	median := Constructor()
-	median.AddNum(1)
-	fmt.Println(median.SmallPile, median.BigPile)
-	median.AddNum(2)
-	fmt.Println(median.SmallPile, median.BigPile)
-	median.AddNum(3)
-	fmt.Println(median.SmallPile, median.BigPile)
-	median.AddNum(7)
-	fmt.Println(median.SmallPile, median.BigPile)
+	median.AddNum(-1)
+	median.AddNum(-2)
+	median.AddNum(-3)
+	median.AddNum(-4)
+	median.AddNum(-5)
+	fmt.Println(median.SmallPile,
+		median.BigPile)
+	fmt.Println(median.FindMedian())
 }
