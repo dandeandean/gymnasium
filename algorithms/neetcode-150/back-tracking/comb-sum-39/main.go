7fab0920202f31888aeb5f4d79f20974ec965dad
DanDeanDean <dpd5518@psu.edu>
2024-11-06 06:52:50 -0500

    MUST REALOCATE with appending to null

--- algorithms/neetcode-150/back-tracking/comb-sum-39/main.go
+++ algorithms/neetcode-150/back-tracking/comb-sum-39/main.go
@@ -8,33 +8,33 @@ type comb struct {
 	target int
 }
 
-func makeComb(cand []int, target int) comb {
-	return comb{
-		res:    make([][]int, 0),
-		cand:   cand,
-		target: target,
-	}
-}
-
 func (c *comb) dfs(i int, cur []int, tot int) {
 	if tot == c.target {
+		fmt.Println(tot, c.res, cur)
 		c.res = append(c.res, cur)
+		fmt.Println(c.res)
 		return
 	}
 	if i >= len(c.cand) || tot > c.target {
 		return
 	}
 	c.dfs(i, append(cur, c.cand[i]), tot+c.cand[i])
-	c.dfs(i+1, cur, tot)
+	c.dfs(i+1, append([]int(nil), cur...), tot)
 }
 
 func combinationSum(candidates []int, target int) [][]int {
-	c := makeComb(candidates, target)
+	c := comb{
+		res:    make([][]int, 0),
+		cand:   candidates,
+		target: target,
+	}
 	c.dfs(0, []int{}, 0)
 	return c.res
 }
+
 func main() {
 	fmt.Println(
-		combinationSum([]int{2, 3, 6, 7}, 7),
+		//combinationSum([]int{2, 3, 6, 7}, 7),
+		combinationSum([]int{2, 3, 5}, 8),
 	)
 }
