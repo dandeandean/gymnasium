ee47acc653c5dd697f743d774a355cda8ba2d3e0
DanDeanDean <dpd5518@psu.edu>
2024-11-06 06:41:38 -0500

    kiss

--- algorithms/neetcode-150/back-tracking/comb-sum-39/main.go
+++ algorithms/neetcode-150/back-tracking/comb-sum-39/main.go
@@ -3,9 +3,6 @@ package main
 import "fmt"
 
 type comb struct {
-	i      int   // candidates[i:] that we can use
-	cur    []int // cur subset we may add to res
-	sum    int   // sum of cur
 	res    [][]int
 	cand   []int
 	target int
@@ -13,55 +10,27 @@ type comb struct {
 
 func makeComb(cand []int, target int) comb {
 	return comb{
-		i:      0,
-		cur:    make([]int, 0),
-		sum:    0,
 		res:    make([][]int, 0),
 		cand:   cand,
 		target: target,
 	}
 }
 
-func (c *comb) append(v int) {
-	c.cur = append(c.cur, v)
-	c.sum = c.sum + v
-}
-
-func (c comb) dump() []int {
-	return append([]int(nil), c.cur...)
-}
-
-func (c comb) take() comb {
-	// copys slices, not cand
-	cNew := c
-	cNew.res = append([][]int(nil), cNew.res...)
-	cNew.cur = append([]int(nil), cNew.cur...)
-	if &cNew.res == &c.res || &cNew.cur == &c.cur {
-		fmt.Println("something horrible happened")
-	}
-	return cNew
-}
-
-func dfs(c comb) {
-	if c.sum == c.target {
-		c.res = append(c.res, c.dump())
+func (c *comb) dfs(i int, cur []int, tot int) {
+	if tot == c.target {
+		c.res = append(c.res, cur)
 		return
 	}
-	if c.i >= len(c.cand) || c.sum > c.target {
+	if i >= len(c.cand) || tot > c.target {
 		return
 	}
-	cLeft := c.take()
-	cLeft.append(c.cand[c.i])
-	dfs(cLeft)
-
-	cRight := c.take()
-	cRight.i += 1
-	dfs(cRight)
+	c.dfs(i, append(cur, c.cand[i]), tot+c.cand[i])
+	c.dfs(i+1, cur, tot)
 }
 
 func combinationSum(candidates []int, target int) [][]int {
 	c := makeComb(candidates, target)
-	dfs(c)
+	c.dfs(0, []int{}, 0)
 	return c.res
 }
 func main() {
