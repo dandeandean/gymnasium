7c7dfccfaf43e2801a7ff42ff5df66db5338ec9a
Dan Dean <dandean44523@gmail.com>
2024-11-09 09:30:30 -0500

    sort Ints

--- algorithms/neetcode-150/back-tracking/subsets-ii-90/main.go
+++ algorithms/neetcode-150/back-tracking/subsets-ii-90/main.go
@@ -16,17 +16,19 @@ func (s *state) curse(i int, subset []int) {
 		s.res = append(s.res, subset)
 		return
 	}
-	sCopy := append([]int(nil), subset...)
-	s.curse(i+1, append(sCopy, s.nums[i]))
+	s.curse(i+1, append(subset, s.nums[i]))
 	for i+1 < len(s.nums) && s.nums[i] == s.nums[i+1] {
 		i++
 	}
-	s.curse(i+1, sCopy)
+	s.curse(i+1, append([]int(nil), subset...))
 }
+
 func subsetsWithDup(nums []int) [][]int {
+	numsSorted := append([]int(nil), nums...)
+	sort.Ints(numsSorted)
 	s := state{
 		res:  make([][]int, 0),
-		nums: sort.IntSlice(nums),
+		nums: numsSorted,
 	}
 	s.curse(0, make([]int, 0))
 	return s.res
@@ -35,6 +37,6 @@ func subsetsWithDup(nums []int) [][]int {
 func main() {
 	fmt.Println(
 		"subsets ii: ",
-		subsetsWithDup([]int{1, 2, 2}),
+		subsetsWithDup([]int{2, 1, 2}),
 	)
 }
