eb74a6e11a46dc06a29a34220c6654c75b44702c
Dan Dean <dandean44523@gmail.com>
2024-11-09 09:21:22 -0500

    add neetcode solution

--- algorithms/neetcode-150/back-tracking/subsets-ii-90/main.go
+++ algorithms/neetcode-150/back-tracking/subsets-ii-90/main.go
@@ -1,28 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 type state struct {
-	res         [][]int
-	nums        []int
-	usedChoices map[int]bool
+	res  [][]int
+	nums []int
 }
 
 func (s *state) curse(i int, subset []int) {
-	if i > len(subset) {
+	if i >= len(s.nums) {
+		fmt.Println(s.nums, subset)
 		s.res = append(s.res, subset)
+		return
 	}
-	sCopy := append([]int{}, subset...)
+	sCopy := append([]int(nil), subset...)
+	s.curse(i+1, append(sCopy, s.nums[i]))
+	for i+1 < len(s.nums) && s.nums[i] == s.nums[i+1] {
+		i++
+	}
 	s.curse(i+1, sCopy)
-	if s.usedChoices[i] == false {
-		s.usedChoices[i] = true
-		s.curse(i, append(sCopy, s.nums[i]))
-	}
 }
 func subsetsWithDup(nums []int) [][]int {
 	s := state{
-		res:         make([][]int, 0),
-		nums:        nums,
-		usedChoices: make(map[int]bool),
+		res:  make([][]int, 0),
+		nums: sort.IntSlice(nums),
 	}
-	s.curse(0, []int{})
+	s.curse(0, make([]int, 0))
 	return s.res
+}
+
+func main() {
+	fmt.Println(
+		"subsets ii: ",
+		subsetsWithDup([]int{1, 2, 2}),
+	)
 }
