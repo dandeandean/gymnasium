a8b3742258b39ca2ffd6e476d7a75a453b74c74d
Dan Dean <dandean44523@gmail.com>
2024-11-14 07:04:22 -0500

    add with nic bug

---
+++ algorithms/neetcode-150/back-tracking/queens-51/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Board []string
+
+func createBoard(n int) *Board {
+	res := make(Board, n)
+	s := ""
+	for i := 0; i < n; i++ {
+		s += "."
+	}
+	for i := 0; i < n; i++ {
+		res[i] = s
+	}
+	return &res
+}
+
+func (b Board) rowClear(r, _ int) bool {
+	for _, ch := range b[r] {
+		if ch != '.' {
+			return false
+		}
+	}
+	return true
+}
+
+func (b Board) colClear(_, c int) bool {
+	for r := range b {
+		if b[r][c] != '.' {
+			return false
+		}
+	}
+	return true
+}
+
+func (b Board) diagLClear(r, c int) bool {
+	for r > 0 && c < len(b[0])-1 { // cast up to the right
+		r--
+		c++
+	}
+	for r < len(b)-1 && c < 0 {
+		if b[r][c] != '.' {
+			return false
+		}
+		r++
+		c--
+	}
+	return true
+}
+
+func (b Board) diagRClear(r, c int) bool {
+	for r > 0 && c < 0 { // cast up to the left
+		r++
+		c--
+	}
+	for r < len(b)-1 && c < len(b[0])-1 {
+		if b[r][c] != '.' {
+			return false
+		}
+		r--
+		c++
+	}
+	return true
+}
+
+func (b Board) canPlaceQ(r, c int) bool {
+	return b.rowClear(r, c) && b.colClear(r, c) &&
+		b.diagRClear(r, c) && b.diagLClear(r, c)
+}
+
+func (b *Board) tryPlaceQ(r, c int) bool {
+	if !b.canPlaceQ(r, c) {
+		return false
+	}
+	byts := []byte((*b)[r])
+	byts[c] = 'Q'
+	(*b)[r] = string(byts)
+	return true
+}
+
+/*
+func diagRClear(r, c int) bool
+func canPlaceQ(r, c int) bool
+func backtrack(n int)
+func solveNQueens(n int) [][]string
+*/
+func main() {
+	b := createBoard(4)
+	b.tryPlaceQ(1, 3)
+	b.tryPlaceQ(0, 1)
+	fmt.Println(b)
+	fmt.Println(
+		b.diagLClear(2, 2),
+	)
+}
