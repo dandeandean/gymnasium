11235f8e6918a2a199830278ddf72485a868e979
Dan Dean <dandean44523@gmail.com>
2024-11-17 10:05:44 -0500

    sloppy solution

--- algorithms/neetcode-150/back-tracking/queens-51/main.go
+++ algorithms/neetcode-150/back-tracking/queens-51/main.go
@@ -4,95 +4,92 @@ import (
 	"fmt"
 )
 
-type Board []string
+type Board struct {
+	b     []string
+	col   map[int]bool
+	row   map[int]bool
+	diagL map[int]bool
+	diagR map[int]bool
+}
 
 func createBoard(n int) *Board {
-	res := make(Board, n)
+	b := make([]string, n)
 	s := ""
 	for i := 0; i < n; i++ {
 		s += "."
 	}
 	for i := 0; i < n; i++ {
-		res[i] = s
+		b[i] = s
 	}
-	return &res
+	return &Board{
+		b:     b,
+		col:   make(map[int]bool),
+		row:   make(map[int]bool),
+		diagL: make(map[int]bool),
+		diagR: make(map[int]bool),
+	}
 }
 
-func (b Board) rowClear(r, _ int) bool {
-	for _, ch := range b[r] {
-		if ch != '.' {
-			return false
-		}
-	}
-	return true
-}
-
-func (b Board) colClear(_, c int) bool {
-	for r := range b {
-		if b[r][c] != '.' {
-			return false
-		}
-	}
-	return true
-}
-
-func (b Board) diagLClear(r, c int) bool {
-	for r > 0 && c < len(b[0])-1 { // cast up to the right
-		r--
-		c++
-	}
-	for r < len(b)-1 && c > 0 {
-		if b[r][c] != '.' {
-			return false
-		}
-		r++
-		c--
-	}
-	return true
-}
-
-func (b Board) diagRClear(r, c int) bool {
-	for r < len(b[0])-1 && c > 0 { // cast up to the right
-		r++
-		c--
-	}
-	for r > 0 && c < len(b[0])-1 {
-		if b[r][c] != '.' {
-			return false
-		}
-		r--
-		c++
-	}
-	return true
-}
-
+func (b Board) rowClear(r, _ int) bool   { return !b.row[r] }
+func (b Board) colClear(_, c int) bool   { return !b.col[c] }
+func (b Board) diagLClear(r, c int) bool { return !b.diagL[r+c] }
+func (b Board) diagRClear(r, c int) bool { return !b.diagR[r-c] }
 func (b Board) canPlaceQ(r, c int) bool {
-	return b.rowClear(r, c) && b.colClear(r, c) &&
-		b.diagRClear(r, c) && b.diagLClear(r, c)
+	return b.rowClear(r, c) && b.colClear(r, c) && b.diagRClear(r, c) && b.diagLClear(r, c)
 }
 
-func (b *Board) tryPlaceQ(r, c int) bool {
+func (b *Board) tryPlaceQ(r, c int) *Board {
 	if !b.canPlaceQ(r, c) {
-		return false
+		return nil
 	}
-	byts := []byte((*b)[r])
-	byts[c] = 'Q'
-	(*b)[r] = string(byts)
-	return true
+	bArr := []byte((*b).b[r])
+	bArr[c] = 'Q'
+	(*b).b[r] = string(bArr)
+	(*b).row[r] = true
+	(*b).col[c] = true
+	(*b).diagL[r+c] = true
+	(*b).diagR[r-c] = true
+	return b
 }
 
-/*
-func diagRClear(r, c int) bool
-func canPlaceQ(r, c int) bool
-func backtrack(n int)
-func solveNQueens(n int) [][]string
-*/
-func main() {
-	b := createBoard(4)
-	for i := range 4 {
-		for j := range 4 {
-			b.tryPlaceQ(i, j)
-		}
-		fmt.Println(b)
+func (b Board) copy() *Board {
+	res := createBoard(len(b.b[0]))
+	copy(res.b, b.b)
+	for k, v := range b.col {
+		res.col[k] = v
 	}
+	for k, v := range b.row {
+		res.row[k] = v
+	}
+	for k, v := range b.diagL {
+		res.diagL[k] = v
+	}
+	for k, v := range b.diagR {
+		res.diagR[k] = v
+	}
+	return res
+}
+
+func solveNQueens(n int) [][]string {
+	board := *createBoard(n)
+	res := make([][]string, 0)
+	var backtrack func(int, Board)
+	backtrack = func(r int, b Board) {
+		if r >= n {
+			res = append(res, b.copy().b)
+			return
+		}
+		for c := range b.b[r] {
+			cp := b.copy().tryPlaceQ(r, c)
+			if cp != nil {
+				backtrack(r+1, *cp)
+			}
+		}
+	}
+	backtrack(0, board)
+	return res
+}
+
+func main() {
+	fmt.Println(solveNQueens(4))
 }
