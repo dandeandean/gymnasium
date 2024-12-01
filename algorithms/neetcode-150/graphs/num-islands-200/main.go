2e375665a03d764dc943373a61b121afa884863b
DanDeanDean <dpd5518@psu.edu>
2024-12-01 09:51:34 -0500

    too slow of a solution:

--- algorithms/neetcode-150/graphs/num-islands-200/main.go
+++ algorithms/neetcode-150/graphs/num-islands-200/main.go
@@ -1,17 +1,85 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	g := [][]byte{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}
-	fmt.Println(numIslands(g))
+	g := [][]byte{
+		{'1', '1', '0', '0'},
+		{'0', '1', '1', '0'},
+		{'0', '0', '0', '0'},
+		{'0', '1', '1', '0'},
+	}
+	fmt.Println(
+		numIslands(g),
+	)
 }
 
-type node struct {
-	isLand    bool
-	neighbors [4]*node
+type coord struct {
+	row  int
+	col  int
+	char byte
+}
+type grid [][]byte
+
+func newGrid(g [][]byte) grid {
+	return g
+}
+
+func (g grid) coordFrom(r, c int) *coord {
+	if r < 0 || c < 0 || r >= len(g) || c >= len(g[0]) {
+		return nil
+	}
+	return &coord{
+		row:  r,
+		col:  c,
+		char: g[r][c],
+	}
+}
+
+func (g grid) getNeighbors(c coord) []coord {
+	dir := [4][2]int{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
+	res := make([]coord, 0)
+	for _, v := range dir {
+		dx, dy := v[0], v[1]
+		n := g.coordFrom(c.row+dx, c.col+dy)
+		if n != nil {
+			res = append(res, *n)
+		}
+	}
+	return res
+}
+func (c coord) isLand() bool {
+	return c.char == '1'
+}
+func (g *grid) bfs(r, c int) {
+	q := make([]*coord, 0)
+	q = append(q, g.coordFrom(r, c))
+	fmt.Println(g)
+	for len(q) > 0 {
+		cur := q[0]
+		q = q[1:]
+		for _, n := range g.getNeighbors(*cur) {
+			if n.isLand() {
+				q = append(q, &n)
+				(*g)[n.row][n.col] = '0'
+			}
+		}
+	}
+	fmt.Println(g)
 }
 
 func numIslands(grid [][]byte) int {
-	return 42
+	g := newGrid(grid)
+	count := 0
+	for r := range g {
+		for c := range g[r] {
+			if g[r][c] == '1' {
+				g.bfs(r, c)
+				count++
+			}
+		}
+	}
+	return count
 }
