cb12e66bdfc6cf867c5da35fa07909f71ad3b376
DanDeanDean <dpd5518@psu.edu>
2024-12-02 05:52:35 -0500

    inf loop :(

--- algorithms/neetcode-150/graphs/max-area-695/main.go
+++ algorithms/neetcode-150/graphs/max-area-695/main.go
@@ -17,23 +17,21 @@ func main() {
 func maxAreaOfIsland(grid [][]int) int {
 	var bfs func(r, c int) int
 	bfs = func(r, c int) int {
-		dirs := [4][2]int{{-1, 0}, {0, -1}, {1, 0}, {0, 1}}
 		if r < 0 || c < 0 || r >= len(grid) || c >= len(grid[r]) {
 			return -1
 		}
-		q := make([][2]int, 0)
+		dirs := [4][2]int{{-1, 0}, {0, -1}, {1, 0}, {0, 1}}
 		count := 0
+		q := [][2]int{{r, c}}
 		for len(q) > 0 {
 			cr, cc := q[0][0], q[0][1]
 			q = q[1:]
 			for _, v := range dirs {
-				dx, dy := v[0], v[1]
-				cNew := cr + dx
-				rNew := cc + dy
-				isValid := cNew > 0 && cNew < len(grid) && rNew > 0 && rNew < len(grid[cNew])
-				if isValid {
-					count += grid[cNew][rNew]
-					grid[cNew][rNew] = 0
+				rNew, cNew := cr+v[0], cc+v[1]
+				isValid := cNew > 0 && cNew < len(grid) &&
+					rNew > 0 && rNew < len(grid[cNew])
+				if isValid && grid[rNew][cNew] != 0 {
+					q = append(q, [2]int{rNew, cNew})
 				}
 			}
 		}
@@ -42,9 +40,11 @@ func maxAreaOfIsland(grid [][]int) int {
 	res := 0
 	for r := range grid {
 		for c := range grid[r] {
-			size := bfs(r, c)
-			if size > res {
-				res = size
+			if grid[r][c] == 1 {
+				size := bfs(r, c)
+				if size > res {
+					res = size
+				}
 			}
 		}
 	}
