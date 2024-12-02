77b59110d91b79fafa6bc2d8a780c8b0da9cdc75
Dan Dean <dandean44523@gmail.com>
2024-12-02 06:31:02 -0500

    DFS solution

--- algorithms/neetcode-150/graphs/max-area-695/main.go
+++ algorithms/neetcode-150/graphs/max-area-695/main.go
@@ -15,33 +15,27 @@ func main() {
 }
 
 func maxAreaOfIsland(grid [][]int) int {
-	var bfs func(r, c int) int
-	bfs = func(r, c int) int {
-		if r < 0 || c < 0 || r >= len(grid) || c >= len(grid[r]) {
-			return -1
+	type coord [2]int
+	var dfsCount func(r, c int) int
+	beenTo := make(map[coord]bool)
+	dfsCount = func(r, c int) int {
+		if r < 0 || c < 0 ||
+			r >= len(grid) || c >= len(grid[r]) ||
+			grid[r][c] == 0 || beenTo[coord{r, c}] {
+			return 0
 		}
-		dirs := [4][2]int{{-1, 0}, {0, -1}, {1, 0}, {0, 1}}
-		count := 0
-		q := [][2]int{{r, c}}
-		for len(q) > 0 {
-			cr, cc := q[0][0], q[0][1]
-			q = q[1:]
-			for _, v := range dirs {
-				rNew, cNew := cr+v[0], cc+v[1]
-				isValid := cNew > 0 && cNew < len(grid) &&
-					rNew > 0 && rNew < len(grid[cNew])
-				if isValid && grid[rNew][cNew] != 0 {
-					q = append(q, [2]int{rNew, cNew})
-				}
-			}
-		}
-		return count
+		beenTo[coord{r, c}] = true
+		return 1 +
+			dfsCount(r+1, c) +
+			dfsCount(r-1, c) +
+			dfsCount(r, c+1) +
+			dfsCount(r, c-1)
 	}
 	res := 0
 	for r := range grid {
 		for c := range grid[r] {
 			if grid[r][c] == 1 {
-				size := bfs(r, c)
+				size := dfsCount(r, c)
 				if size > res {
 					res = size
 				}
