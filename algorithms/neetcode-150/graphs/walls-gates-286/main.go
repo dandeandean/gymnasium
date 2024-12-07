a67ba92beb2dc0adbb153ecaab22b254c49b057a
DanDeanDean <dpd5518@psu.edu>
2024-12-07 08:06:03 -0500

    no idea what's going on here, moving on

--- algorithms/neetcode-150/graphs/walls-gates-286/main.go
+++ algorithms/neetcode-150/graphs/walls-gates-286/main.go
@@ -23,15 +23,34 @@ func main() {
 
 func islandsAndTreasure(grid [][]int) {
 	q := make(qu, 0)
+	g := newGrid(grid)
 	for r := range grid {
 		for c := range grid[r] {
-			if grid[r][c] == 0 {
-				q.push(coord{r, c})
+			co := coord{r, c}
+			if g.isTreasure(co) {
+				q.push(co)
 			}
 		}
 	}
-	fmt.Println(q)
-
+	dirs := [4]coord{
+		{-1, 0}, {0, -1},
+		{0, 1}, {1, 0},
+	}
+	for len(q) > 0 {
+		co := q.pop()
+		for _, d := range dirs {
+			co2 := coord{
+				co[0] + d[0],
+				co[1] + d[1],
+			}
+			fmt.Println(co, co2)
+			if g.canWalk(co2) {
+				q.push(co2)
+				g.set(co2, g[co[0]][co[1]]+1)
+			}
+		}
+	}
+	g.show()
 }
 
 var (
@@ -44,6 +63,16 @@ type grid [][]int
 type coord [2]int
 type qu []coord
 
+func newGrid(g [][]int) grid {
+	return g
+}
+
+func (g grid) show() {
+	for r := range g {
+		fmt.Println(g[r])
+	}
+}
+
 func (g grid) isValid(c coord) bool {
 	if c[0] >= len(g) || c[0] < 0 {
 		return false
@@ -55,26 +84,35 @@ func (g grid) isValid(c coord) bool {
 }
 
 func (g grid) canWalk(c coord) bool {
-	return g.isValid(c) && g[c[0]][c[1]] == GOAL
+	return g.isValid(c) && g[c[0]][c[1]] != WALL
 }
 
 func (g grid) isTreasure(c coord) bool {
 	return g.isValid(c) && g[c[0]][c[1]] == GOAL
 }
 
+func (g grid) get(c coord) int {
+	if g.isValid(c) {
+		fmt.Println("Can't get", c, " in")
+		g.show()
+		panic("Invalid coord!")
+	}
+	return g[c[0]][c[1]]
+}
+
 func (g *grid) set(c coord, n int) bool {
 	if !g.canWalk(c) {
-		(*g)[c[0]][c[1]] = n
+		return false
 	}
+	(*g)[c[0]][c[1]] = n
 	return (*g)[c[0]][c[1]] == n
 }
 
-func (q *qu) push(n coord) *qu {
+func (q *qu) push(n coord) {
 	*q = append(*q, n)
-	return q
 }
-func (q *qu) pop() (coord, *qu) {
+func (q *qu) pop() coord {
 	out := (*q)[0]
 	*q = (*q)[1:]
-	return out, q
+	return out
 }
