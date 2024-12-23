6acee09cffccd5ce446b41d10c0a146054e80203
DanDeanDean <dpd5518@psu.edu>
2024-12-23 11:41:09 -0500

    rm return

--- algorithms/neetcode-150/graphs/valid-tree-261/main.go
+++ algorithms/neetcode-150/graphs/valid-tree-261/main.go
@@ -7,7 +7,6 @@ func main() {
 		validTree(5, [][]int{{0, 1}, {0, 2}, {0, 3}, {1, 4}}),
 		validTree(5, [][]int{{0, 1}, {1, 2}, {2, 3}, {1, 3}, {1, 4}}),
 	)
-	return
 }
 
 func validTree(n int, edges [][]int) bool {
