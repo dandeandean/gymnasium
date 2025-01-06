544b87a2a04ca6135b5b07b96ad388b27a7e91eb
DanDeanDean <dpd5518@psu.edu>
2025-01-06 06:41:28 -0500

    working

--- algorithms/neetcode-150/graphs-ii/network-743/main.go
+++ algorithms/neetcode-150/graphs-ii/network-743/main.go
@@ -1,36 +1,65 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("hello world")
+	g := [][]int{{2, 1, 1}, {2, 3, 1}, {3, 4, 1}}
+	fmt.Println(networkDelayTime(g, 4, 2))
 }
 
-type node []int
 type heapPair struct {
-	node *node
-	dist int
+	nodeId int
+	dist   int
+}
+type minHeap []heapPair
+
+func (h minHeap) Len() int           { return len(h) }
+func (h minHeap) Less(i, j int) bool { return h[i].dist < h[j].dist }
+func (h minHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *minHeap) Push(x any) {
+	*h = append(*h, x.(heapPair))
+}
+
+func (h *minHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
 
 func networkDelayTime(times [][]int, n int, k int) int {
-	edges := make(map[int][]node, 0)
+	edges := make(map[int][][]int, 0)
 	for _, t := range times {
-		edges[t[0]] = append(edges[t[0]], node{t[1], t[2]})
+		edges[t[0]] = append(edges[t[0]], []int{t[1], t[2]})
 	}
-	q := []int{k}
+	mh := &minHeap{heapPair{k, 0}}
+	heap.Init(mh)
 	beenTo := make([]bool, n+1)
-	for len(q) != 0 {
-		cur := q[0]
-		q = q[1:]
-		for _, nei := range edges[cur] {
-			if !beenTo[cur] {
-				beenTo[cur] = true
-				q = append(q, nei[0])
-			}
+	counter := 0
+	for mh.Len() > 0 {
+		cur := heap.Pop(mh).(heapPair)
+		curNode, curDist := cur.nodeId, cur.dist
+		if beenTo[curNode] {
+			continue
 		}
-
+		if curDist > counter {
+			counter = curDist
+		}
+		beenTo[curNode] = true
+		for _, nei := range edges[cur.nodeId] {
+			heap.Push(mh, heapPair{nei[0], curDist + nei[1]})
+		}
 	}
-	return 69
+	fmt.Println(beenTo)
+	for _, v := range beenTo[1:] {
+		if !v { // return -1 if not visited
+			return -1
+		}
+	}
+	return counter
 }
