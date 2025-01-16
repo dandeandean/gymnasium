ff3ca1c3f4ccf674354c730c47928c1eb90d34d6
dandeandean <dandean44523@gmail.com>
2025-01-16 06:59:51 -0500

    started on hiers algor

--- algorithms/neetcode-150/graphs-ii/itinerary-332/main.go
+++ algorithms/neetcode-150/graphs-ii/itinerary-332/main.go
@@ -22,36 +22,13 @@ func findItinerary(tickets [][]string) []string {
 		aList[t[0]] = append(aList[t[0]], t[1])
 	}
 	for k := range aList {
-		sort.Strings(aList[k])
+		sort.Sort(sort.Reverse(sort.StringSlice(aList[k])))
 	}
-	res := []string{"JFK"}
-	var dfs func(string) bool
-	dfs = func(w string) bool {
-		if len(res) == len(tickets)+1 {
-			return true
-		}
-
-		nextPlaces, ok := aList[w]
-		if !ok {
-			return false
-		}
-
-		temp := make([]string, len(nextPlaces))
-		copy(temp, nextPlaces)
-
-		for i, str := range temp {
-			aList[w] = append(aList[w][0:i], aList[w][i+1:]...)
-			res = append(res, str)
-			if dfs(str) {
-				return true
-			}
-			front, back := aList[w][:i], aList[w][i:]
-			aList[w] = append(front, append([]string{str}, back...)...)
-			res = res[:len(res)-1]
-		}
-		return false
+	res := make([]string, 0)
+	stack := []string{"JFK"}
+	for len(stack) > 0 {
+		cur := stack[len(stack)-1]
+		res = append(res, cur)
 	}
-	dfs("JFK")
-
 	return res
 }
