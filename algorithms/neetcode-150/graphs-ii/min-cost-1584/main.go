0e5e694394670cb8ed09202024712659f23510f6
DanDeanDean <dpd5518@psu.edu>
2025-01-26 14:44:50 -0500

    copy solution

--- algorithms/neetcode-150/graphs-ii/min-cost-1584/main.go
+++ algorithms/neetcode-150/graphs-ii/min-cost-1584/main.go
@@ -1,76 +1,47 @@
 package main
 
-import "container/heap"
-
 // An IntHeap is a min-heap of ints.
-type IntHeap [][]int
-
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i][0] < h[j][0] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *IntHeap) Push(x any)        { *h = append(*h, x.([]int)) }
-func (h *IntHeap) Pop() any {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
-
-type Point struct {
-	n int
-	x int
-	y int
-}
-
-func PointsFrom(points [][]int) []Point {
-	pRepr := make([]Point, 0)
-	for n, p := range points {
-		pNew := Point{
-			n: n,
-			x: p[0],
-			y: p[1],
-		}
-		pRepr = append(pRepr, pNew)
-	}
-	return pRepr
-}
-
-func (p Point) distTo(p2 Point) int {
-	a := p.x - p2.x
-	if a < 0 {
-		a = -a
-	}
-	b := p.y - p2.y
-	if b < 0 {
-		b = -b
-	}
-	return a + b
-}
 
 func minCostConnectPoints(points [][]int) int {
-	frontier := &IntHeap{}
-	heap.Init(frontier)
-	pRepr := PointsFrom(points)
-	aList := make(map[Point]*IntHeap)
-	for a, p1 := range pRepr {
-		for b, p2 := range pRepr {
-			dist := p1.distTo(p2)
-			heap.Push(aList[pRepr[a]], []int{dist, p2.n})
-			heap.Push(aList[pRepr[b]], []int{dist, p1.n})
+	manDist := func(i, j int) int {
+		distX := points[i][0] - points[j][0]
+		distY := points[i][1] - points[j][1]
+		if distX < 0 {
+			distX = -distX
 		}
-	}
-	//Prims alg
-	beenTo := make(map[int]bool)
-	// loop over the outgoing edges
-	cur := &pRepr[0]
-	for cur != nil {
-		beenTo[cur.n] = true
-		outgoingEdge := heap.Pop(aList[*cur]).([]int)
-		for beenTo[outgoingEdge[1]] {
-			outgoingEdge = heap.Pop(aList[*cur]).([]int)
+		if distY < 0 {
+			distY = -distY
 		}
-		cur = &pRepr[outgoingEdge[1]]
+		return distX + distY
 	}
-	return points[0][0]
+	distTo := make([]int, len(points))
+	for i := range points {
+		distTo[i] = 99999999999999999
+	}
+	beenTo := make([]bool, len(points))
+	cur, edgeCount, res := 0, 0, 0
+	for edgeCount < len(points)-1 {
+		beenTo[cur] = true
+		next := -1
+
+		// all possible connections
+		for i := range points {
+			if beenTo[i] {
+				continue
+			}
+
+			d := manDist(cur, i)
+			if d < distTo[i] {
+				distTo[i] = d
+			}
+
+			if next == -1 || distTo[i] < distTo[next] {
+				next = i
+			}
+		}
+		res += distTo[next]
+		cur = next
+		edgeCount++
+	}
+	return res
 }
