0346d5617dd239e0e07d9ed4f2416fc5a5dd53ee
dandeandean <dandean44523@gmail.com>
2025-01-31 06:41:31 -0500

    traverse a bit

--- algorithms/neetcode-150/graphs-ii/aliens-269/main.go
+++ algorithms/neetcode-150/graphs-ii/aliens-269/main.go
@@ -2,5 +2,13 @@ package main
 
 func AlienDictinary(words []string) string {
 	// return their lexographical ordering
+	//adjList := make(map[byte]map[byte]bool)
+	for i := range len(words) - 1 {
+		w1, w2 := words[i], words[i+1]
+		smallerLen := len(w1)
+		if len(w2) < smallerLen {
+			smallerLen = len(w2)
+		}
+	}
 	return words[0]
 }
