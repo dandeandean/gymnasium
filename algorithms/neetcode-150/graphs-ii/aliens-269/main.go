2f4371ed83f001b503c75e5a52c0fedd985cd994
DanDeanDean <dpd5518@psu.edu>
2025-02-01 10:28:02 -0500

    set up set I think

--- algorithms/neetcode-150/graphs-ii/aliens-269/main.go
+++ algorithms/neetcode-150/graphs-ii/aliens-269/main.go
@@ -1,8 +1,30 @@
 package main
 
+import "fmt"
+
+func main() {
+	test1 := []string{"hrn", "hrf", "er", "enn", "rfnn"}
+	test2 := []string{"z", "o"}
+	fmt.Println(
+		AlienDictinary(test1),
+		AlienDictinary(test2),
+	)
+}
+
 func AlienDictinary(words []string) string {
 	// return their lexographical ordering
-	//adjList := make(map[byte]map[byte]bool)
+	type set map[rune]bool
+	type cList map[rune]set
+	// we need a mapping of rune -> rune
+	// to reprsent an adj list
+	wList := make(map[string]cList)
+	for _, w := range words {
+		wList[w] = make(cList)
+		for _, r := range w {
+			wList[w][r] = make(set)
+		}
+	}
+
 	for i := range len(words) - 1 {
 		w1, w2 := words[i], words[i+1]
 		smallerLen := len(w1)
