2c2c437e9094dcafe11926103223e325bc2ca290
dandeandean <dandean44523@gmail.com>
2025-02-03 06:57:33 -0500

    move along

--- algorithms/neetcode-150/graphs-ii/aliens-269/main.go
+++ algorithms/neetcode-150/graphs-ii/aliens-269/main.go
@@ -13,15 +13,11 @@ func main() {
 
 func AlienDictinary(words []string) string {
 	// return their lexographical ordering
-	type set map[rune]bool
-	type cList map[rune]set
-	// we need a mapping of rune -> rune
 	// to reprsent an adj list
-	wList := make(map[string]cList)
+	comesB4 := make(map[byte]map[byte]bool)
 	for _, w := range words {
-		wList[w] = make(cList)
 		for _, r := range w {
-			wList[w][r] = make(set)
+			comesB4[byte(r)] = make(map[byte]bool)
 		}
 	}
 
@@ -31,6 +27,26 @@ func AlienDictinary(words []string) string {
 		if len(w2) < smallerLen {
 			smallerLen = len(w2)
 		}
+		if len(w1) > len(w2) && w1[:smallerLen] == w2[:smallerLen] {
+			return ""
+		}
+		for j := range smallerLen {
+			if w1[j] != w2[j] {
+				comesB4[w1[j]][w2[j]] = true
+				break
+			}
+		}
 	}
+
+	visited := make(map[byte]bool)
+	var dfs func(c byte) bool
+	dfs = func(c byte) bool {
+		if visited[c] {
+			return visited[c]
+		}
+		visited[c] = true
+		return true
+	}
+	dfs(words[0][0])
 	return words[0]
 }
